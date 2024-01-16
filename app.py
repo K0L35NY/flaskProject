e7635e20e46b8e9595e726334188d3440796f390
K0L35NY <mateuszkolodziej@protonmail.com>
2024-01-16 12:21:12 +0100

    Initial commit

---
+++ app.py
@@ -0,0 +1,69 @@
+from flask import Flask, render_template, request, redirect, url_for, flash
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
+
+app = Flask(__name__)
+app.secret_key = "klucz_tajny"
+
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(120), nullable=False)
+
+
+with app.app_context():
+
+    db.create_all()
+
+
+def save_data():
+    with app.app_context():
+        db.session.commit()
+
+
+@app.route('/')
+def home():
+    return 'Witaj! To jest strona główna aplikacji.'
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        user = User.query.filter_by(username=username).first()
+
+        if user and check_password_hash(user.password, password):
+            flash('Zalogowano pomyślnie!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Nieprawidłowa nazwa użytkownika lub hasło!', 'danger')
+
+    return render_template('login.html')
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        hashed_password = generate_password_hash(password, method='sha256')
+
+        new_user = User(username=username, password=hashed_password)
+        db.session.add(new_user)
+        save_data()
+        flash('Konto zostało utworzone pomyślnie! Możesz się teraz zalogować.', 'success')
+        return redirect(url_for('login'))
+
+    return render_template('register.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
